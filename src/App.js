2aadce55f52756bfc6f736e24ea2a5f75b12a61b
ihab-sensei <ib-mashhadani@hotmail.com>
2020-09-26 20:32:53 +0300

    Added some components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
-
+import Table from "./components/pages/inventory/Table";
 function App() {
   return (
     <div className="App">
       <h1>Tina</h1>
+      <Table />
     </div>
   );
 }
