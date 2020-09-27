314a5d5e8ec6d9e0975185b77dcf484b4be32b9c
ihab-sensei <ib-mashhadani@hotmail.com>
2020-09-27 14:09:23 +0300

    Fixed order styling

--- src/components/Orders/Order.js
+++ src/components/Orders/Order.js
@@ -1,24 +1,25 @@
 import { Label } from "@material-ui/icons";
-import React, {useState} from "react";
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import "./style.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& .MuiTextField-root': {
+    "& .MuiTextField-root": {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: "25ch",
     },
   },
 }));
 
 export default function Orders() {
   const classes = useStyles();
-  const [form, setForm] = useState({forms : ["form0"]});
+  const [form, setForm] = useState({ forms: ["form0"] });
   const [input, setInput] = useState({
-    code : "",
-    quantity : ""
+    code: "",
+    quantity: "",
   });
   const [quantity, setQuantity] = useState(0);
 
@@ -32,29 +33,50 @@ export default function Orders() {
     e.preventDefault();
   };
   const addNewFood = (e) => {
-    e.persist()
+    e.persist();
     const newForm = `form${form.forms.length}`;
     setForm((prevState) => ({ forms: prevState.forms.concat([newForm]) }));
   };
   return (
-    <div style={{marginTop:"1rem"}}>
+    <div className="ordersContainer">
       <h3>Enter Order</h3>
-      <div>      
-        {form.forms.map((el) => <form className={classes.root} noValidate autoComplete="off"
-         key={el} onSubmit={handleSubmit}>
-         
-        
-          <TextField id="outlined-helperText" helperText="Enter Food Code" variant="outlined"
-            name={"code" + el} value={input.code} onChange={(e) => handleChange(e)}/>
-         
-        
-          <TextField id="outlined-helperText" helperText="Enter Quantity" variant="outlined"
-           type="text" name={"quantity" + el} value={input.quantity}  onChange={(e) => handleChange(e)}/>
+      <div>
+        {form.forms.map((el) => (
+          <form
+            className={classes.root}
+            noValidate
+            autoComplete="off"
+            key={el}
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              id="outlined-helperText"
+              helperText="Enter Food Code"
+              variant="outlined"
+              name={"code" + el}
+              value={input.code}
+              onChange={(e) => handleChange(e)}
+            />
 
-          <Button onClick={addNewFood} variant="contained" color="primary">Add New Food</Button>
-          <Button variant="contained" color="secondary">Submit</Button>
-      </form>)}
-    </div>
+            <TextField
+              id="outlined-helperText"
+              helperText="Enter Quantity"
+              variant="outlined"
+              type="text"
+              name={"quantity" + el}
+              value={input.quantity}
+              onChange={(e) => handleChange(e)}
+            />
+
+            <Button onClick={addNewFood} variant="contained" color="primary">
+              Add New Food
+            </Button>
+            <Button variant="contained" color="secondary">
+              Submit
+            </Button>
+          </form>
+        ))}
+      </div>
     </div>
   );
 }
