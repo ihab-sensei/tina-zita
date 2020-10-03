f10411a118fb114dd9ff627b241db282ab10503d
ihab-sensei <ib-mashhadani@hotmail.com>
2020-10-03 09:22:48 +0300

    Updates

---
+++ precache-manifest.5f084bb7e95754766c62b451b2a3089b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefe1bc48e7bb66d4aa95656c08dc264",
+    "url": "/tina-zita/index.html"
+  },
+  {
+    "revision": "9c6c47ad222f1ccf590f",
+    "url": "/tina-zita/static/css/main.66b87390.chunk.css"
+  },
+  {
+    "revision": "103ac10340ae0bcd226f",
+    "url": "/tina-zita/static/js/2.30ca790f.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/tina-zita/static/js/2.30ca790f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6c47ad222f1ccf590f",
+    "url": "/tina-zita/static/js/main.85ce5fc3.chunk.js"
+  },
+  {
+    "revision": "c3a9c9caee67dc4d90dc",
+    "url": "/tina-zita/static/js/runtime-main.f60e141e.js"
+  }
+]);
